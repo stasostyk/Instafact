6ba887d2ec4c48c2025b59e3b54b70beea31b878
stashito <s.ostyks@gmail.com>
2021-08-10 01:26:06 -0500

    wait for promise before parsing

--- js/webscraper.js
+++ js/webscraper.js
@@ -2,17 +2,31 @@
 var KEY = "AIzaSyB8mjsz6uOQVW3OWZYTAot0REBajNrMrIA";
 var CX = "2285ce8a05b90d2be"
 
-getSearchResults("banana");
+var query_term = "banana";
 
+// accessing promised data with .then() to wait for response first
+getSearchResults(query_term).then(function(results) {
+  var titles = [];
+  var links = [];
+
+  // print results while parsing through results and organizing them into title and link arrays
+  console.log("=== Search Results for '" + query_term + "' ===")
+  for (var i = 0; i < results.length; i++) {
+    titles.push(results[i].title);
+    links.push(results[i].link);
+    console.log ((i+1) + ". '" + titles[i] + "'");
+    console.log ("    link -> " + links[i]);
+  }
+
+  // boom, now we have two arrays of search results
+
+
+});
+
+// requesting data from Google Search API, returning list of GoogleSearch objects
 function getSearchResults(query) {
-  $.getJSON('https://www.googleapis.com/customsearch/v1?key='+KEY+'&cx='+CX+'&q='+query, function(resp)
-  {
-    var i=0;
-    for (var i = 0; i < 10; i++) {
-      var title=resp.items[i].title;
-      var link=resp.items[i].link;
-
-      alert(title);
-    }
-  });
-}
+  return $.getJSON(
+    'https://www.googleapis.com/customsearch/v1?key='+KEY+'&cx='+CX+'&q='+query).then(function(data) {
+      return data.items;
+    });
+  }
